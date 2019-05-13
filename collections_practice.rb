cecd84036135d7c7ec1c8abcae362bde277e1c0b
Stella McEwen <mcewen.stella@gmail.com>
2019-05-13 20:17:41 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,57 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+
+def sort_array_desc(array)
+  array.sort.reverse
+end
+
+
+def sort_array_char_count(array)
+array.sort_by{|string| string.length}
+end
+
+
+def swap_elements(array)
+  array[1], array[2] = array [2], array[1]
+  array
+end
+
+
+def reverse_array(array)
+  array.reverse 
+end
+
+
+def kesha_maker(array)
+  keshaed_strings = []
+  i = 0
+  array.each do |string|
+    string[2] = "$"
+    keshaed_strings << array[i]
+    i += 1 
+  end
+  keshaed_strings
+end
+ 
+ 
+ def find_a(array)
+   a_words = []
+   array.map do |word|
+     if word.start_with?("a")
+       a_words << word 
+     end 
+   end
+     a_words
+   end
+   
+   
+   def sum_array(array)
+     array.inject(:+)
+   end
+   
+   
+   def add_s(array)
+     array.each_with_index.collect
+   end
