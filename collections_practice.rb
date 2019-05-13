92fc5daa63192c5ef829895909966bc1145ea4e9
Stella McEwen <mcewen.stella@gmail.com>
2019-05-13 20:19:00 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -54,5 +54,9 @@ end
    
    def add_s(array)
      array.each_with_index.collect {|element, index|}
-     if index != 1
+     if index != 1 
+       element + "s"
+     end
+     array
+   end
    end
